03c2523dd77616985fb3ef22ab70da042e999422
박소정 <126138033+sojeong0302@users.noreply.github.com>
2023-11-22 04:49:37 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+let N = parseInt(input[0]);
+
+for (let i = 1; i <= 9; i++) {
+  console.log(`${N} * ${i} = ${N * i}`);
+}
