b71b83531042d81cdfa59c4879ebcf84e3523d37
박소정 <126138033+sojeong0302@users.noreply.github.com>
2023-11-23 20:11:04 +0900

    [Bronze II] Title: 공 바꾸기, Time: 180 ms, Memory: 9372 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+
+let arr = Array(N)
+  .fill(1)
+  .map((a, b) => a + b);
+
+for (let i = 1; i <= M; i++) {
+  let [x, y] = input[i].split(" ").map(Number);
+  let tmp = arr[x - 1];
+  arr[x - 1] = arr[y - 1];
+  arr[y - 1] = tmp;
+}
+
+console.log(arr.join(" "));
