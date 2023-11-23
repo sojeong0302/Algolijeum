db9e980c37488d639dd9ac14d357a0db6e742221
박소정 <126138033+sojeong0302@users.noreply.github.com>
2023-11-23 20:17:53 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 136 ms, Memory: 9440 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+
+let basket = Array(N)
+  .fill(1)
+  .map((a, b) => a + b);
+
+for (let i = 1; i <= M; i++) {
+  let [a, b] = input[i].split(" ").map(Number);
+  let arr = [];
+
+  for (let j = a - 1; j < b; j++) {
+    arr.push(basket[j]);
+  }
+
+  arr.reverse();
+  basket.splice(a - 1, b - a + 1, ...arr);
+}
+
+console.log(basket.join(" "));
