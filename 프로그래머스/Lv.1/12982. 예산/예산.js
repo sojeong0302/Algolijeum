d721e5a1dad3d7ecc1cf8ded8572a5d381264b92
박소정 <126138033+sojeong0302@users.noreply.github.com>
2024-01-16 16:19:03 +0900

    [level Lv.1] Title: 예산, Time: 3.87 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+function solution(d, budget) {
+    d.sort((a,b)=>a-b);
+    let count=0;
+    for(let i = 0; i < d.length; i++){
+        if(budget - d[i] >= 0){
+            budget -= d[i];
+            count++;
+        }
+    }
+    console.log(budget)
+    return count;
+}
